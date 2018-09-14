b8c2b2b8fb7c287c52d2197f32875c9fbf90829c
Shel Kong <kongscn@gmail.com>
2018-09-15 01:24:41 +0800

    Prepare v0.2
    Inject a `ecplot` method to pandas DataFrame and Series.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ecdf",
-    version="0.1",
+    version="0.2",
     author="Shel Kong",
     author_email="kongscn@gmail.com",
     description="Visualize dataframes with echarts",
